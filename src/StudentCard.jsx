b4e89613e3996daf418fa2c4ac0bc1bf75b35d6c
sristygiri123-afk <sristygiri123-afk@gmail.com>
2026-07-13 14:10:18 +0530

    changes in style

--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 
 function StudentCard(std) {
   return (
-    <div>
+    <div style={{
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "30vh"
+}}>
+  <div style={{
+    border: "1px solid black",
+    borderRadius: "10px",
+    padding: "15px",
+    margin: "10px",
+    width: "250px",
+    backgroundColor: "#877878",
+    color: "black"
+  }}>
       <h2>{std.name}</h2>
       <p>Age: {std.age}</p>
       <p>Grade: {std.grade}</p>
       <p>Course: {std.course}</p>
-    </div>
+
+  </div>
+</div>
+
   );
 }
 
 export default StudentCard;
-
 
 
 
